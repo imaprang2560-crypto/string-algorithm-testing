3aef639e443d14b4e47403074a6a74a9f134dacc
KorawanLeelertwongsakul <korawanstar06@gmail.com>
2026-02-21 11:09:18 +0700

    add caesar

---
+++ caesar.py
@@ -0,0 +1,21 @@
+def caesar_cipher(s,k):
+
+    result=""
+
+    k=k%26
+
+    for char in s:
+
+        if char.islower():
+
+            result+=chr((ord(char)-97+k)%26+97)
+
+        elif char.isupper():
+
+            result+=chr((ord(char)-65+k)%26+65)
+
+        else:
+
+            result+=char
+
+    return result
