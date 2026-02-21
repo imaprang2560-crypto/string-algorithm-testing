a6cbb46727249e2cf6d8119d0a306135f3806693
KorawanLeelertwongsakul <korawanstar06@gmail.com>
2026-02-21 11:11:45 +0700

    add grid

---
+++ grid.py
@@ -0,0 +1,13 @@
+def grid_challenge(grid):
+
+    grid=[''.join(sorted(row)) for row in grid]
+
+    for col in range(len(grid[0])):
+
+        for row in range(len(grid)-1):
+
+            if grid[row][col]>grid[row+1][col]:
+
+                return "NO"
+
+    return "YES"
